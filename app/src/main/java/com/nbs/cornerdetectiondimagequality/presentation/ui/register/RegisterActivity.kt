67d724d5c83569a2f9d0778beae1439990edc8c8
mharun1 <harunriyad23@gmail.com>
2024-12-08 13:36:05 +0700

    feat(alert dialog): Add alert dialog after successfully make pin

--- app/src/main/java/com/nbs/cornerdetectiondimagequality/presentation/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/nbs/cornerdetectiondimagequality/presentation/ui/register/RegisterActivity.kt
@@ -1,12 +1,22 @@
 package com.nbs.cornerdetectiondimagequality.presentation.ui.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.nbs.cornerdetectiondimagequality.R
 import com.nbs.cornerdetectiondimagequality.databinding.ActivityRegisterBinding
+import com.nbs.cornerdetectiondimagequality.presentation.ui.login.LoginActivity
 import com.nbs.cornerdetectiondimagequality.presentation.viewmodel.AuthViewModel
 import com.nbs.cornerdetectiondimagequality.presentation.viewmodel.ViewModelFactory
 import com.ozcanalasalvar.otp_view.view.OtpView
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -25,14 +35,23 @@ class RegisterActivity : AppCompatActivity() {
         binding.textfieldPin.setTextChangeListener(object : OtpView.ChangeListener{
             override fun onTextChange(value: String, completed: Boolean) {
                 if (completed){
-
-                    viewModel.savePin(value)
-                    finish()
+                    lifecycleScope.launch {
+                        val dialog = showDialog()
+                        delay(3000)
+                        dialog.dismiss()
+                        viewModel.savePin(value)
+                        startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+                        finish()
+                    }
                 }
             }
         })
+    }
 
-
-
+    private fun showDialog() : AlertDialog {
+        return MaterialAlertDialogBuilder(this).apply {
+            setTitle(R.string.alert_title)
+            setMessage(R.string.alert_message)
+        }.show()
     }
 }
